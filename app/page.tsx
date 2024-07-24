e6ab2658ceac381b52eea709878ab7e6b30d3755
monangbahana <monangbahana@gmail.com>
2024-07-24 15:56:44 +0000

    patienForm 50%

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import Image from "next/image";
 import { Button } from "./components/ui/button";
+import PatientForm from "./components/forms/PatientForm";
+import Link from "next/link";
 
 export default function Home() {
   return(
@@ -13,8 +17,27 @@ export default function Home() {
             width={1000}
             className="mb-12 h-10 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-item-end text-dark-600 xl:text-left">
+              © 2024 HealtCare
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image 
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="Patient"
+      className="side-img max-w-[50%]"
+      />
     </div>
   )
 }
